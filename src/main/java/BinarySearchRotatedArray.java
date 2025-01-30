8f997e69ca69cff5b5627a24b011dcf183fb7cbd
stanley <coronavirusvaccinexx1@gmail.com>
2025-01-30 13:24:32 -0500

    implement search

--- src/main/java/BinarySearchRotatedArray.java
+++ src/main/java/BinarySearchRotatedArray.java
@@ -14,6 +14,60 @@ public class BinarySearchRotatedArray {
          * that means the array is not rotated at all or
          * the smallest index is at position 0.(remember the array is sorted)
          */
+
+
+
+        if (rotationIndex == 0) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == target) {
+                    return i;
+                }
+            }
+        }
+
+
+
+        if (rotationIndex != 0) {
+            if (arr[rotationIndex] == target) {
+                return rotationIndex;
+            }
+
+            int x = arr.length - rotationIndex;
+            if (arr[rotationIndex + x - 1] < target) {
+                int right = arr.length - 1;
+                int left = 0;
+                while (left <= right) {
+                    int m = left + (right - left) / 2;
+                    if (arr[m] > target) {
+                        right = m - 1;
+                    } else if (arr[m] < target) {
+                        left = m + 1;
+                    } else {
+                        return m;
+                    }
+                }
+
+                //        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+//        assertEquals(6, searcher.search(arr, 2, 4));
+            } else if (arr[rotationIndex + x - 1] > target) {
+                int left = rotationIndex;
+                int right = arr.length - 1;
+                while (left <= right) {
+                    int m = left + (right - left) / 2;
+                    if (arr[m] > target) {
+                        right = m - 1;
+                    } else if (arr[m] < target) {
+                        left = m + 1;
+                    } else {
+                        return m;
+                    }
+                }
+            }
+
+        }
+
+
+        return -1;
         
     }
 
@@ -27,8 +81,17 @@ public class BinarySearchRotatedArray {
      * @return        The index of the target value if found, -1 otherwise
      */
     private int binarySearch(int[] arr, int left, int right, int target){
-        
+        while (left <= right) {
+            int m = left + (right - left) / 2;
+            if (arr[m] > target) {
+                right = m - 1;
+            } else if (arr[m] < target) {
+                left = m + 1;
+            } else {
+                return m;
+            }
+        }
+
         return -1;
-        
     }
 }
